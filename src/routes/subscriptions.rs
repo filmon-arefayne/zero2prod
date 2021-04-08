3d5de79113dd1b0401404f5c2b42a2774de9b076
Filmon97 <filmon67@hotmail.it>
2021-04-08 23:19:44 +0200

    Add tracing instead of log

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,7 +13,24 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     db_pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
-    log::info!("Saving new subscriber details in the database");
+    let request_id = Uuid::new_v4();
+    // by using % sigil we are telling `tracing` to use the fmt::Display implementation
+    // let request_span = tracing::info_span!(
+    //     "Adding a new subscriber",
+    //     %request_id,
+    //     email = %form.email,
+    //     name = %form.name
+    // );
+    tracing::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name,
+    );
+    tracing::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -27,9 +44,13 @@ pub async fn subscribe(
     .execute(db_pool.get_ref())
     .await
     .map_err(|e| {
-        log::error!("Failed to execute query: {:?}", e);
+        tracing::error!(
+            "request_id {} - Failed to execute query: {:?}",
+            request_id,
+            e
+        );
         HttpResponse::InternalServerError().finish()
     })?;
-    log::info!("New subscriber details have been saved");
+    tracing::info!("New subscriber details have been saved");
     Ok(HttpResponse::Ok().finish())
 }
