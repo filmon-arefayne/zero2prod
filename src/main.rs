a89c4b5b5631a587e98f8d68e40a5d4533575765
Filmon97 <filmon67@hotmail.it>
2021-04-12 11:12:40 +0200

    Add tracing-buyan-formatter

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,25 @@ use std::net::TcpListener;
 use sqlx::PgPool;
 use zero2prod::configuration;
 use zero2prod::startup;
-
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "zero2prod".into(),
+        std::io::stdout
+    );
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
