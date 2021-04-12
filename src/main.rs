eaae1b5a065b7e6b876a9cbbad09673829d1bd6f
Filmon97 <filmon67@hotmail.it>
2021-04-12 12:43:05 +0200

    Add telemetry module

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,12 @@ use std::net::TcpListener;
 use sqlx::PgPool;
 use zero2prod::configuration;
 use zero2prod::startup;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use tracing_log::LogTracer;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    
-    LogTracer::init().expect("Failed to set logger");
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        std::io::stdout
-    );
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    
-    set_global_default(subscriber).expect("Failed to set subscriber");
-
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
@@ -31,5 +15,6 @@ async fn main() -> std::io::Result<()> {
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind port");
 
-    startup::run(listener, connection_pool)?.await
+    startup::run(listener, connection_pool)?.await?;
+    Ok(())
 }
