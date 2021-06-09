968fcaa98b417ef46a1102bff8014c157ac3e4e1
Filmon97 <filmon67@hotmail.it>
2021-06-09 09:31:30 +0200

    Add subscribe_persists_the_new_subscriber test

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -55,9 +55,11 @@ pub async fn subscribe(
     name = "Send a confirmation email to a new subscriber",
     skip(email_client, new_subscriber)
 )]
-pub async fn send_confirmation_email(email_client: &EmailClient, new_subscriber: NewSubscriber) -> Result<(), reqwest::Error> {
-    let confirmation_link = 
-        "https://my-api.com/subscriptions/confirm";
+pub async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link = "https://my-api.com/subscriptions/confirm";
     let plain_body = format!(
         "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
         confirmation_link
@@ -68,12 +70,8 @@ pub async fn send_confirmation_email(email_client: &EmailClient, new_subscriber:
         confirmation_link
     );
     email_client
-        .send_email(
-            new_subscriber.email,
-            "Welcome!",
-            &html_body,
-            &plain_body,
-        ).await
+        .send_email(new_subscriber.email, "Welcome!", &html_body, &plain_body)
+        .await
 }
 
 #[tracing::instrument(
@@ -87,7 +85,7 @@ pub async fn insert_subscriber(
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-        VALUES ($1, $2, $3, $4, 'confirmed')
+        VALUES ($1, $2, $3, $4, 'pending_confirmation')
         "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
