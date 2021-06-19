2410eb8187e9ef35ca99288c1a571d0b043a915f
Filmon97 <filmon67@hotmail.it>
2021-06-19 17:19:53 +0200

    Chain the error messages

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -171,7 +171,7 @@ impl std::fmt::Display for StoreTokenError {
 
 impl std::fmt::Debug for StoreTokenError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}\nCaused by:\n\t{}", self, self.0)
+        error_chain_fmt(self, f)
     }
 }
 impl ResponseError for StoreTokenError {}
@@ -182,3 +182,15 @@ impl std::error::Error for StoreTokenError {
     }
 }
 
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
+}
